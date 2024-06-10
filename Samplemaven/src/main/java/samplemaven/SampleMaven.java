eb5a7a263738d02e15fb6bbaf367c829839c1bbd
saveri <saveri@RATZZ>
2024-06-10 21:18:22 +0530

    push project

---
+++ Samplemaven/src/main/java/samplemaven/SampleMaven.java
@@ -0,0 +1,12 @@
+package samplemaven;
+
+public class SampleMaven 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
